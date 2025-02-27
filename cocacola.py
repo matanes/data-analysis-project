35274c26631d1b3923f67f9cf0030469f94a22b8
matanes <marioatanes@gmail.com>
2025-02-27 20:22:16 +0000

    cambio version

--- cocacola.py
+++ cocacola.py
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 print("cocacola")git status fadsfadsf fgsgfdggdd aaaaaaaaa
+=======
+print("cocacola") bbbbbbbbbb dfsfs dfsdfddgsgsdfgsdfgsdgfgfdgdfsgsdfgfdsgsfdgsfdgfdsgsdfgfsdg
+>>>>>>> Stashed changes
